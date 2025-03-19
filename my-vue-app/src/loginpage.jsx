456119e8f24c3041fe43a9adf56878fd0030743c
abhishek <bhshkvijayan@gmail.com>
2025-03-19 09:02:58 +0530

    first commit

--- my-vue-app/src/loginpage.jsx
+++ my-vue-app/src/loginpage.jsx
@@ -1,23 +1,11 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React from 'react'
 
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-
-
-function App() {
-
+function loginpage() {
   return (
     <div style={{ backgroundColor: "#007bff", height: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
     <div className="d-flex justify-content-center align-items-center" style={{ width: "400px", height: "auto", backgroundColor: "white", borderRadius: "10px", padding: "20px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)" }}>
       <Form>
-        <h4 className="text-center mb-4">Signup page</h4>
-        <Form.Group className="mb-3" controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter your name" />
-        </Form.Group>
-
+        <h4 className="text-center mb-4">signup Page</h4>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
@@ -31,15 +19,13 @@ function App() {
         <Button variant="primary" type="submit" className="w-100">
           Login
         </Button>
-
         <div className="text-center mt-3">
-          <p>Already have an account?<a href="#">Log in</a></p>
+          <p>Don't have an account? <a href="#">Sign Up</a></p>
         </div>
       </Form>
     </div>
   </div>
-  );
-  
+  )
 }
 
-export default App
+export default loginpage
